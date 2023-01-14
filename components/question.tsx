8c599d139c511bbfe8b1b6bae40b5424cfd4667d
Haffi Mazhar <haffimazhar96@gmail.com>
2023-01-14 22:52:36 +0000

    Use animations

--- components/question.tsx
+++ components/question.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from "next/link";
 import type { QuestionData } from "../types";
+import { motion } from "framer-motion";
 
 interface QuestionProps {
   question: QuestionData
@@ -12,9 +15,9 @@ export default function Question({ question }: QuestionProps) {
     <>
       <div className="py-2 flex w-1/2 justify-center">
         <Link href={`/questions/${id}`} className="flex w-full justify-center">
-          <div className="flex w-full p-5 bg-blue-200 hover:bg-blue-500 dark:bg-red-300 dark:hover:bg-red-500 rounded-full">
+          <motion.button whileHover={{ scale: 1.05, transition: { duration: 0.3 } }} className="flex w-full p-5 bg-blue-200 hover:bg-blue-500 dark:bg-red-300 dark:hover:bg-red-500 rounded-full">
             <p>{title}</p>
-          </div>
+          </motion.button>
         </Link>
 
       </div>
