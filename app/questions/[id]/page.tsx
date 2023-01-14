e3ea770cf90ace071e7c9175cb2f7f201e79a542
bilalm354 <bilalm354@gmail.com>
2023-01-14 23:16:17 +0000

    minor refactor to  questions id page

--- app/questions/[id]/page.tsx
+++ app/questions/[id]/page.tsx
@@ -4,10 +4,16 @@ import { RadioGroup } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import { QuestionData } from "../../../types";
 
+const CHECKED_STYLE = "pl-3 p-5 bg-blue-400 dark:bg-red-500 rounded-full flex";
+const UNCHECKED_STYLE = "pl-3 p-5 bg-blue-200 dark:bg-red-300 rounded-full flex w-11/12";
 
+interface QuestionPageParams {
+  params: {
+    id: string
+  }
+}
 
-
-export default function QuestionPage({ params }: any) {
+export default function QuestionPage({ params }: QuestionPageParams) {
   const [questionData, setQuestion] = useState<QuestionData>();
   const [checked, setChecked] = useState<string>("");
 
@@ -16,16 +22,13 @@ export default function QuestionPage({ params }: any) {
       const res = await fetch(
         `${process.env.PB_API}/collections/questions/records/${params.id}`,
         { cache: "no-store" }
-        );
-        const data = await res.json();
+      );
+      const data = await res.json();
 
-        setQuestion(data);
-      };
-      getQuestion();
-    }, [params.id]);
-
-    const CHECKED_STYLE = "pl-3 p-5 bg-blue-400 dark:bg-red-500 rounded-full flex";
-    const UNCHECKED_STYLE = "pl-3 p-5 bg-blue-200 dark:bg-red-300 rounded-full flex w-11/12";
+      setQuestion(data);
+    };
+    getQuestion();
+  }, [params.id]);
 
   return (
     <div>
@@ -33,7 +36,7 @@ export default function QuestionPage({ params }: any) {
         <p className="font-bold">{questionData?.title}</p>
         <p className="py-5">{questionData?.body}</p>
         <RadioGroup value={checked} onChange={setChecked} className="pt-3 flex flex-col w-2/3 space-y-3">
-          {/* <RadioGroup.Label>Pick an answer:</RadioGroup.Label> */}
+          <RadioGroup.Label>Pick an answer:</RadioGroup.Label>
           <RadioGroup.Option value="a1">
             {({ checked }) => (
               <span className={checked ? CHECKED_STYLE : UNCHECKED_STYLE}>
@@ -57,9 +60,9 @@ export default function QuestionPage({ params }: any) {
           </RadioGroup.Option>
         </RadioGroup>
         <div className="p-10">
-        <button className="bg-transparent hover:bg-blue-500 dark:hover:bg-red-500 text-blue-700 dark:text-red-700 font-semibold hover:text-white dark:hover:text-white py-2 px-4 border border-blue-500 dark:border-red-500 hover:border-transparent rounded">
-          Submit
-        </button>
+          <button className="bg-transparent hover:bg-blue-500 dark:hover:bg-red-500 text-blue-700 dark:text-red-700 font-semibold hover:text-white dark:hover:text-white py-2 px-4 border border-blue-500 dark:border-red-500 hover:border-transparent rounded">
+            Submit
+          </button>
         </div>
       </div>
     </div>
