956cd20365beb44806ab6920a453076ba6e89ad0
Haffi Mazhar <haffimazhar96@gmail.com>
2023-01-15 19:03:49 +0000

    refactor answer request and darktheme

---
+++ app/questions/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default async function Loading() {
+    return (
+        <>
+            <title>Loading..</title>
+            <link rel="icon" href="/quiz.svg" />
+            <h1>Loading...</h1>
+        </>
+    );
+}
--- helpers/databaseHelper.ts
+++ helpers/databaseHelper.ts
@@ -1,11 +1,11 @@
 import PocketBase from 'pocketbase';
-import type { QuestionsRecord, QuestionsResponse } from "../pocketbase-types";
+import type { QuestionsRecord, QuestionsResponse, AnswersResponse } from "../pocketbase-types";
 import { Collections } from "../pocketbase-types";
 
 const pb = new PocketBase(process.env.PB_URL);
 pb.autoCancellation(false);
 
-export async function getQuestion(id:string) {
+export async function getQuestion(id: string) {
     return await pb.collection(Collections.Questions).getOne<QuestionsResponse>(id);
 }
 
@@ -17,6 +17,10 @@ export async function createQuestion(bodyParms: QuestionsRecord) {
     return await pb.collection(Collections.Questions).create<QuestionsRecord>(bodyParms);
 }
 
-export async function editQuestion(id:string, bodyParms: QuestionsRecord) {
+export async function editQuestion(id: string, bodyParms: QuestionsRecord) {
     return await pb.collection(Collections.Questions).update<QuestionsRecord>(id, bodyParms);
+}
+
+export async function getAnswer(questionID: string) {
+    return await pb.collection(Collections.Answers).getFirstListItem<AnswersResponse>(`question_id="${questionID}"`);
 }
