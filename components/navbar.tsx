5adbe90dfaf02e8a60d8a416ecc5f9399b2f0b45
bilalm354 <bilalm354@gmail.com>
2023-01-21 15:13:21 +0000

    refactored nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,52 +2,24 @@ import Link from "next/link";
 import LogOut from "./LogOutButton";
 import Switcher from "./Switcher";
 
-
 export default function NavBar({ accessToken }: { accessToken?: string }) {
+  const LoggedInContents = (
+    <>
+      <Link href="/admin/questions" className="absolute top-5 left-5">Admin</Link>
+      <Link href="/questions/all" className="px-3">Questions</Link>
+      <LogOut />
+    </>
+  )
 
-  const Nav = () => {
-    if (accessToken) {
-      return (
-        <LoggedInNavBar />
-      )
-    } else {
-      return (<LoggedOutNavBar />)
-    }
-  }
-
-  const LoggedInNavBar = () => {
-    return (
-      <>
-        <nav className="flex flex-row bg-black text-white justify-center p-5 w-full">
-          <Link href="/" className="px-3">Home</Link>
-          <Link href="/admin/questions" className="absolute top-5 left-5">Admin</Link>
-          <Link href="/questions/all" className="px-3">Questions</Link>
-          <LogOut />
-          <div className="absolute top-5 right-5">
-            <Switcher />
-          </div>
-        </nav>
-      </>
-    );
-  }
-
-  const LoggedOutNavBar = () => {
-    return (
-      <>
-        <nav className="flex flex-row bg-black text-white justify-center p-5 w-full">
-          <Link href="/" className="px-3">Home</Link>
-          <Link href="/login">Login</Link>
-          <div className="absolute top-5 right-5">
-            <Switcher />
-          </div>
-        </nav>
-      </>
-    );
-  }
+  const LoggedOutContents = <Link href="/login">Login</Link>
 
   return (
-    <>
-      <Nav />
-    </>
-  );
+    <nav className="flex flex-row bg-black text-white justify-center p-5 w-full">
+      <Link href="/" className="px-3">Home</Link>
+      {accessToken ? LoggedInContents : LoggedOutContents}
+      <div className="absolute top-5 right-5">
+        <Switcher />
+      </div>
+    </nav>
+  )
 }
