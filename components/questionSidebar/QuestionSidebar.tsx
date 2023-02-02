1c88259d1104a47f5ff2b9090751fa3772a4807c
Haffi Mazhar <haffimazhar96@gmail.com>
2023-02-02 22:48:24 +0000

    add search to sidebar

--- components/questionSidebar/QuestionSidebar.tsx
+++ components/questionSidebar/QuestionSidebar.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import Link from "next/link";
 import type { Database } from "../../lib/database.types";
 import { Routes } from "../../enums/Routes";
 import { QuestionSidebarListItem } from "./QuestionSidebarListItem";
+import { useState } from "react";
 
 interface QuestionSidebarProps {
     allQuestions: Database["public"]["Tables"]["questions"]["Row"][],
@@ -9,9 +12,19 @@ interface QuestionSidebarProps {
 }
 
 export default function QuestionSidebar({ allQuestions, route }: QuestionSidebarProps) {
+    const [search, setSearch] = useState('')
+
     return (
         <aside className="mx-auto w-full lg:left-0 lg:m-0 lg:h-full lg:w-1/3">
-            <div className="overflow-y-auto rounded-lg bg-gray-50 dark:bg-gray-800 lg:h-full ">
+            <div className="overflow-y-auto rounded-lg bg-gray-50 dark:bg-gray-800 lg:h-full">
+                <div className="pt-10 text-center">
+                    <input
+                        className="h-10 w-4/5 rounded-lg bg-white px-2 text-black"
+                        type="text"
+                        placeholder="Search questions"
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
                 <button className="lg:hidden">+/-</button>
                 <ul className="space-y-2 p-10">
                     <Link href={route} className="mb-5 flex items-center pl-2.5">
@@ -20,7 +33,11 @@ export default function QuestionSidebar({ allQuestions, route }: QuestionSidebar
                     {route === Routes.ADMIN_EDIT && <QuestionSidebarListItem newItem text="# Create New Question #" key={Routes.NEW_QUESTION} index={-1} href={Routes.NEW_QUESTION} />}
                     <br />
                     {
-                        allQuestions.map((question, index) => (
+                        allQuestions.filter((question) => {
+                            return search === ''
+                                ? question
+                                : question.title.includes(search)
+                        }).map((question, index) => (
                             <QuestionSidebarListItem text={question.title ?? 'Missing title'} key={question.id} index={index} href={`${route}/${question.id}`} />
                         ))
                     }
