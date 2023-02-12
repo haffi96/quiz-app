6c77e246f50607037f6cb6083e3fc58ea0696a51
bilalm354 <bilalm354@gmail.com>
2023-02-12 08:05:56 -0500

    improved navbar mobile styling

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -9,6 +9,7 @@ import AccountMenuDropDown from "../MenuDropDown";
 export default function NavBar({ accessToken }: { accessToken?: string }) {
   const pathname = usePathname();
   const [isHidden, setIsHidden] = useState(true);
+  const isLoggedIn = Boolean(accessToken);
 
   useEffect(() => {
     setIsHidden(true);
@@ -21,7 +22,6 @@ export default function NavBar({ accessToken }: { accessToken?: string }) {
       <NavBarListItem><Link href="/pricing">Pricing</Link></NavBarListItem>
       <NavBarListItem><Link href='/admin/questions'>Admin</Link></NavBarListItem>
       <NavBarListItem><Link href='/questions/all'>Questions</Link></NavBarListItem>
-      <NavBarListItem><AccountMenuDropDown /></NavBarListItem>
     </>
   )
 
@@ -37,15 +37,16 @@ export default function NavBar({ accessToken }: { accessToken?: string }) {
   }
 
   return (
-    <nav className="sticky top-0 z-50 w-full bg-black text-left text-white">
+    <nav className="sticky top-0 z-50 w-full bg-black text-white">
       <button type="button" onClick={onClick} className="ml-3 inline-flex items-center p-2 lg:hidden" aria-controls="navbar-default" aria-expanded="false">
         <svg className="h-32 w-32" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
       </button>
+      {isLoggedIn && <AccountMenuDropDown />}
 
       <div className={'justify-center lg:flex lg:justify-center' + maybeAppendHiddenStyle()}>
-        <ul className="flex-col pb-4 text-center lg:flex lg:flex-row lg:p-5">
+        <ul className="flex-col text-center lg:flex lg:flex-row lg:p-5">
           <NavBarListItem><Link href='/'>Home</Link></NavBarListItem>
-          {accessToken ? LoggedInContents : LoggedOutContents}
+          {isLoggedIn ? LoggedInContents : LoggedOutContents}
         </ul>
       </div>
     </nav>
--- components/navBar/NavBarListItem.tsx
+++ components/navBar/NavBarListItem.tsx
@@ -7,5 +7,5 @@ interface NavBarListItemProps {
 }
 
 export function NavBarListItem({ children }: NavBarListItemProps) {
-    return <li className="m-auto lg:p-0 lg:px-3">{children}</li>;
+    return <li className="mb-8 py-8 lg:m-0 lg:p-0 lg:px-3">{children}</li>;
 }
