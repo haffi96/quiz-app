8a0a55e5c050af35901424816756564db4cd34d2
bilalm354 <bilalm354@gmail.com>
2023-02-15 18:59:59 -0500

    fix build error

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -17,16 +17,6 @@ async function getAllQuestionSets() {
 
     return data
 }
-
-export async function addQuestionSetToUserSubscriptions(currentQuestionSetIds: number[], questionSetId: number) {
-    await supabaseBrowser.from('users').update({ 'subscribed_to_question_sets': [...currentQuestionSetIds, questionSetId] })
-}
-
-export async function removeQuestionSetToUserSubscriptions(currentQuestionSetIds: number[], questionSetId: number) {
-    const newArray = [...currentQuestionSetIds].filter((id) => id !== questionSetId);
-    await supabaseBrowser.from('users').update({ 'subscribed_to_question_sets': newArray })
-}
-
 export interface SubsciptionSectionProps {
     subscribedQuestionSetIds: number[],
     questionSets: Database['public']['Tables']['question_sets']['Row'][]
